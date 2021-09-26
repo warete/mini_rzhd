99f5e31d51b8ffe37d270af8da195e8ce9458314
Egor Borisovsky <egor@intervolga.ru>
2021-09-26 19:19:31 +0300

    Доделал главную и поиск

---
+++ src/Controller/TicketController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Station;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TicketController extends AbstractController
+{
+	/**
+	 * @Route("/")
+	 * @return Response
+	 */
+	public function main(ManagerRegistry $managerRegistry, Request $request): Response
+	{
+		$entityManager = $managerRegistry->getManager();
+
+		$stations = $entityManager->getRepository(Station::class)
+			->findAll();
+
+		/*$stationFrom = $entityManager->getRepository(Station::class)->find(1);
+		$stationTo = $entityManager->getRepository(Station::class)->find(5);
+
+		$route = (new \App\Entity\Route())
+			->setStationFrom($stationFrom)
+			->setStationTo($stationTo)
+			->setPrice(3499)
+			->setDateStart(new \DateTime())
+			->setDateEnd(new \DateTime());
+		$entityManager->persist($route);
+		$entityManager->flush();
+		die();*/
+
+		$routes = [];
+		if ($request->get('station_from') && $request->get('station_to'))
+		{
+			$routes = $entityManager->getRepository(\App\Entity\Route::class)
+				->findByStationsInDates(
+					intval($request->get('station_from')),
+					intval($request->get('station_to')),
+					$request->get('date_start'),
+					$request->get('date_end')
+				);
+		}
+
+		return $this->render('main.html.twig', [
+			'stations' => $stations,
+			'routes' => $routes,
+			'searchValues' => [
+				'station_from' => $request->get('station_from'),
+				'station_to' => $request->get('station_to'),
+				'date_start' => $request->get('date_start'),
+				'date_end' => $request->get('date_end'),
+			],
+			'isSearchStart' => $request->get('q') == 'y'
+		]);
+	}
+}
