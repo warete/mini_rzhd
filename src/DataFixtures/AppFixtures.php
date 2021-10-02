e9373e5a4d83cb9391e6ea397be4b2d52a871a17
Egor Borisovsky <mr-warete@mail.ru>
2021-10-03 00:17:22 +0300

    Фикстура для пользователей и билетов. Добавил в проект faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,15 +3,34 @@
 namespace App\DataFixtures;
 
 use App\Entity\Station;
+use App\Entity\Ticket;
 use App\Entity\Train;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+	protected $faker;
+
     public function load(ObjectManager $manager)
     {
+    	$this->faker = Factory::create('ru_RU');
+
+    	$users = [];
+    	for ($i = 0; $i < $this->faker->numberBetween(5, 15); $i++)
+		{
+			[$firstName, $secondName, $lastName] = explode(' ', $this->faker->name);
+			$user = (new User())
+				->setName($firstName)
+				->setLastName($lastName)
+				->setSecondName($secondName);
+			$manager->persist($user);
+			$users[] = $user;
+		}
+
 		$stations = [];
 		foreach ([
 					 'Волгоград 1',
@@ -31,7 +50,7 @@ class AppFixtures extends Fixture
 		for ($i = 0; $i < 20; $i++)
 		{
 			$train = (new Train())->setNumber(uniqid())
-				->setMaxSeatsCnt(random_int(30, 50));
+				->setMaxSeatsCnt($this->faker->numberBetween(30, 50));
 			$manager->persist($train);
 			$trains[] = $train;
 		}
@@ -44,20 +63,28 @@ class AppFixtures extends Fixture
 				{
 					continue;
 				}
-				for ($o = 0; $o < random_int(1, 5); $o++)
+				for ($o = 0; $o < $this->faker->numberBetween(1, 5); $o++)
 				{
 					$route = (new \App\Entity\Route())
 						->setStationFrom($stationFrom)
 						->setStationTo($stationTo)
-						->setPrice(random_int(1000, 7000))
-						->setDateStart((new \DateTime())->modify(sprintf('-% days', random_int(0, 10))))
-						->setDateEnd((new \DateTime())->modify(sprintf('+% days', random_int(0, 10))));
-					$trainIndexStart = random_int(0, count($stations) - 1);
-					for ($k = $trainIndexStart; $k < $trainIndexStart + 5; $k++)
+						->setPrice($this->faker->numberBetween(1000, 7000))
+						->setDateStart((new \DateTime())->modify(sprintf('-% days', $this->faker->numberBetween(0, 10))))
+						->setDateEnd((new \DateTime())->modify(sprintf('+% days', $this->faker->numberBetween(0, 10))));
+					foreach ($this->faker->randomElements($trains, 5) as $train)
 					{
-						$route->addTrain($trains[$k]);
+						$route->addTrain($train);
 					}
 					$manager->persist($route);
+
+					for ($l = 0; $l < $this->faker->numberBetween(0, 10); $l++)
+					{
+						$ticket = (new Ticket())
+							->setIsPaid($this->faker->boolean())
+							->setUser($this->faker->randomElement($users))
+							->setRoute($route);
+						$manager->persist($ticket);
+					}
 				}
 			}
 		}
